949cc5ad5dad27c71f245d5bc1587e6f6a296388
fabuss254 <fabuss254@outlook.fr>
2018-05-23 21:14:43 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -3,11 +3,10 @@
 /* VARIABLES */
 
 const Discord = require("discord.js");
-//const fs = require("fs");
 
+const auth = require("./Bot_Modules/Auth.json");
 const Preferences = require("./Bot_Modules/Settings.json");
 const Details = require("./Bot_Modules/Bots_Details.json");
-//var GuildsData = JSON.parse(fs.readFileSync("./Guild_Save/Settings.json", "utf8"));
 var prefix = Preferences.Prefix;
 var footer = Preferences.Footer_Embed;
 var IntervalMode = 1;
@@ -41,6 +40,12 @@ bot.on('ready', ()=> {
 
 bot.login(process.env.TOKEN);
 console.log("Login succesfully!");
+
+bot.on("guildDelete", LeavedGuild => {
+    if (GuildsData[CreatedGuild.id]) {
+        GuildsData[CreatedGuild.id] = null
+    };
+});
 
 bot.on('message', message => {
     if (message.author.bot) return;
@@ -86,43 +91,6 @@ bot.on('message', message => {
                 message.channel.send(bot_embed);
                 break;
 
-            case "config":
-                if (!args[1]){
-                    var bot_embed = new Discord.RichEmbed()
-                    .setColor("#06FAB2")
-                    .addField(
-                        "Configurateurs:",
-                         "SetLogChannel: Regler le channel de log sur le channel actuelle" +
-                         "\nGetLogChannel: Avoir le channel de log pour ce serveur"
-                    )
-                    .setFooter(footer);
-                    message.channel.send(bot_embed);
-                }else{
-                    switch(args[1].toLowerCase()){
-                        case "setlogchannel":
-                            /** 
-                            GuildsData[message.guild.id].LogChannel = message.channel
-
-                            fs.writeFile("./Guild_Save/Settings.json", JSON.stringify(GuildsData), (err) => {
-                                if (err) console.error(err)
-                            });
-
-                            message.channel.send("Channel de log regler a " + message.channel)
-                            **/
-                            break;
-                            
-                        case "getlogchannel":
-                            if (!GuildsData[message.guild.id] || !GuildsData[message.guild.id].LogChannel){
-                                message.channel.send("Il n'y a pas de channel de log pour ce serveur")
-                            }else{
-                                message.channel.send("Channel de log pour ce serveur: " + GuildsData[message.guild.id].LogChannel)
-                            }
-
-                            break;
-                    };
-                };
-                break;
-
         };
     };
 };
@@ -161,17 +129,6 @@ function OnInterval(TotalUser2, TotalGuild2){
         bot.user.setPresence({game:{name: prefix + "help", url: "https://www.twitch.tv/fabuss255", type: 1}}).then(IntervalMode = 1).catch(console.error);
     };
 }
-/**
-function MakeNewConfig(ID){
-    GuildsData[ID] = {
-        LogChannel = null
-    }
-
-    fs.writeFile("./Guild_Save/Settings.json", JSON.stringify(GuildsData), (err) => {
-        if (err) console.error(err)
-    });
-}
-**/
 
 bot.on("error", err => {
     console.log(err);
